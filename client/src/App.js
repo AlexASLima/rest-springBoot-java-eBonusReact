fcec0678e261c71eb1d652a7a98b7139c456ff4c
Alex Lima <alex.lima@jfl.com.br>
2025-08-01 11:21:39 -0300

    Carregando projeto.

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Header from './Header'
+
+export default function App() {
+  return (
+    // <Header title="Client REST Udemy"/>
+    // Abaixo outra forma de passar propriedade, diferente da acima. São duas formas
+
+    <Header>Client REST Udemy by Alex</Header>
+  );
+}
