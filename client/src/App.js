05d066fa75ef521af45df66d841a22b0bb337837
Alex Lima <alex.lima@jfl.com.br>
2025-08-06 11:38:16 -0300

    Conceito de estado e imutabilidade.

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import Header from './Header'
 
 export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter +1)
+  }
+
   return (
     // <Header title="Client REST Udemy"/>
     // Abaixo outra forma de passar propriedade, diferente da acima. São duas formas
 
-    <Header>Client REST Udemy by Alex</Header>
+    //<Header>Client REST Udemy by Alex</Header>
+
+    <div>
+      <Header>
+        Counter: {counter}
+      </Header>
+      <button onClick={increment}>Add</button>
+    </div>
+
   );
 }
