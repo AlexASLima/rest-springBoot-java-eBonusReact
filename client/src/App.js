b232241b9b325f65fca8c43578429898fc155494
Alex Lima <alex.lima@jfl.com.br>
2026-01-15 16:02:19 -0300

    Iniciando a implementacao do login.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-import Header from './Header'
+import './global.css'
+
+import Login from './pages/Login';
 
 export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  function increment(){
-    setCounter(counter +1)
-  }
-
   return (
-    // <Header title="Client REST Udemy"/>
-    // Abaixo outra forma de passar propriedade, diferente da acima. São duas formas
-
-    //<Header>Client REST Udemy by Alex</Header>
-
-    <div>
-      <Header>
-        Counter: {counter}
-      </Header>
-      <button onClick={increment}>Add</button>
-    </div>
-
+    <Login/>
   );
 }
